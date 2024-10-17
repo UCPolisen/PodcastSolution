b1a4586dacfe542f8baabaf8ae29114ad1df4110
UCPolisen <168416050+UCPolisen@users.noreply.github.com>
2024-10-17 11:51:53 +0200

    a

--- PodcastProjekt/Form1.Designer.cs
+++ PodcastProjekt/Form1.Designer.cs
@@ -55,6 +55,7 @@
             Column3 = new DataGridViewTextBoxColumn();
             Column4 = new DataGridViewTextBoxColumn();
             Column5 = new DataGridViewTextBoxColumn();
+            label8 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
@@ -62,7 +63,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(32, 40);
+            label1.Location = new Point(32, 100);
             label1.Name = "label1";
             label1.Size = new Size(109, 32);
             label1.TabIndex = 0;
@@ -71,15 +72,15 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(147, 37);
+            textBox1.Location = new Point(147, 100);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(231, 39);
+            textBox1.Size = new Size(289, 39);
             textBox1.TabIndex = 1;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(32, 100);
+            label2.Location = new Point(32, 177);
             label2.Name = "label2";
             label2.Size = new Size(84, 32);
             label2.TabIndex = 2;
@@ -87,15 +88,15 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(146, 100);
+            textBox2.Location = new Point(146, 170);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(200, 39);
+            textBox2.Size = new Size(290, 39);
             textBox2.TabIndex = 3;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(32, 158);
+            label3.Location = new Point(32, 257);
             label3.Name = "label3";
             label3.Size = new Size(108, 32);
             label3.TabIndex = 4;
@@ -104,38 +105,41 @@
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(146, 158);
+            comboBox1.Location = new Point(146, 257);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(242, 40);
+            comboBox1.Size = new Size(290, 40);
             comboBox1.TabIndex = 5;
             // 
             // label4
             // 
             label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
             label4.Location = new Point(32, 503);
             label4.Name = "label4";
-            label4.Size = new Size(110, 32);
+            label4.Size = new Size(119, 32);
             label4.TabIndex = 7;
-            label4.Text = "Podcasts:";
+            label4.Text = "Podcasts";
             // 
             // label5
             // 
             label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
             label5.Location = new Point(921, 503);
             label5.Name = "label5";
-            label5.Size = new Size(92, 32);
+            label5.Size = new Size(100, 32);
             label5.TabIndex = 8;
-            label5.Text = "Avsnitt:";
+            label5.Text = "Avsnitt";
             label5.Click += label5_Click;
             // 
             // label6
             // 
             label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
             label6.Location = new Point(507, 37);
             label6.Name = "label6";
-            label6.Size = new Size(129, 32);
+            label6.Size = new Size(137, 32);
             label6.TabIndex = 10;
-            label6.Text = "Kategorier:";
+            label6.Text = "Kategorier";
             // 
             // listBox3
             // 
@@ -174,7 +178,7 @@
             // 
             // button4
             // 
-            button4.Location = new Point(32, 231);
+            button4.Location = new Point(117, 345);
             button4.Name = "button4";
             button4.Size = new Size(197, 47);
             button4.TabIndex = 15;
@@ -211,11 +215,12 @@
             // label7
             // 
             label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
             label7.Location = new Point(1200, 40);
             label7.Name = "label7";
-            label7.Size = new Size(142, 32);
+            label7.Size = new Size(153, 32);
             label7.TabIndex = 19;
-            label7.Text = "Beskrivning:";
+            label7.Text = "Beskrivning";
             // 
             // listBox4
             // 
@@ -289,11 +294,23 @@
             Column5.Name = "Column5";
             Column5.Width = 600;
             // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(32, 18);
+            label8.Name = "label8";
+            label8.Size = new Size(299, 45);
+            label8.TabIndex = 24;
+            label8.Text = "Grupp 25 Podcast";
+            label8.Click += label8_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1654, 1045);
+            Controls.Add(label8);
             Controls.Add(dataGridView2);
             Controls.Add(dataGridView1);
             Controls.Add(button8);
@@ -354,5 +371,6 @@
         private DataGridViewTextBoxColumn Column3;
         private DataGridViewTextBoxColumn Column4;
         private DataGridViewTextBoxColumn Column5;
+        private Label label8;
     }
 }
--- PodcastProjekt/Form1.cs
+++ PodcastProjekt/Form1.cs
@@ -21,5 +21,10 @@ namespace PodcastProjekt
         {
 
         }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
