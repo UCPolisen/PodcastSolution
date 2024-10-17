1eee4e1a19f694a04b1d7e0657bc9255156f5e0a
UCPolisen <168416050+UCPolisen@users.noreply.github.com>
2024-10-17 11:08:25 +0200

    a

--- PodcastProjekt/Form1.Designer.cs
+++ PodcastProjekt/Form1.Designer.cs
@@ -28,12 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            listBox1 = new ListBox();
+            label4 = new Label();
+            label5 = new Label();
+            listBox2 = new ListBox();
+            label6 = new Label();
+            listBox3 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            label7 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(32, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 32);
+            label1.TabIndex = 0;
+            label1.Text = "RSS/URL:";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(147, 37);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(231, 39);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(32, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 32);
+            label2.TabIndex = 2;
+            label2.Text = "Namn:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(146, 100);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(200, 39);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(32, 158);
+            label3.Name = "label3";
+            label3.Size = new Size(108, 32);
+            label3.TabIndex = 4;
+            label3.Text = "Kategori:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(146, 158);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(242, 40);
+            comboBox1.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(32, 564);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(722, 356);
+            listBox1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(32, 503);
+            label4.Name = "label4";
+            label4.Size = new Size(110, 32);
+            label4.TabIndex = 7;
+            label4.Text = "Podcasts:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(921, 503);
+            label5.Name = "label5";
+            label5.Size = new Size(92, 32);
+            label5.TabIndex = 8;
+            label5.Text = "Avsnitt:";
+            label5.Click += label5_Click;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(921, 564);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(633, 356);
+            listBox2.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(507, 37);
+            label6.Name = "label6";
+            label6.Size = new Size(129, 32);
+            label6.TabIndex = 10;
+            label6.Text = "Kategorier:";
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.Location = new Point(507, 100);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(323, 292);
+            listBox3.TabIndex = 11;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(881, 100);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 12;
+            button1.Text = "Lägg till";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(881, 170);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 46);
+            button2.TabIndex = 13;
+            button2.Text = "Ta bort";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(881, 243);
+            button3.Name = "button3";
+            button3.Size = new Size(150, 46);
+            button3.TabIndex = 14;
+            button3.Text = "Uppdatera";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(32, 231);
+            button4.Name = "button4";
+            button4.Size = new Size(197, 47);
+            button4.TabIndex = 15;
+            button4.Text = "Lägg till podcast";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(32, 957);
+            button5.Name = "button5";
+            button5.Size = new Size(185, 46);
+            button5.TabIndex = 16;
+            button5.Text = "Ändra Namn";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(247, 957);
+            button6.Name = "button6";
+            button6.Size = new Size(150, 46);
+            button6.TabIndex = 17;
+            button6.Text = "Ta bort";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(881, 318);
+            button7.Name = "button7";
+            button7.Size = new Size(150, 74);
+            button7.TabIndex = 18;
+            button7.Text = "Ändra namn";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(1200, 40);
+            label7.Name = "label7";
+            label7.Size = new Size(142, 32);
+            label7.TabIndex = 19;
+            label7.Text = "Beskrivning:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1654, 1045);
+            Controls.Add(label7);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listBox3);
+            Controls.Add(label6);
+            Controls.Add(listBox2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(listBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private ComboBox comboBox1;
+        private ListBox listBox1;
+        private Label label4;
+        private Label label5;
+        private ListBox listBox2;
+        private Label label6;
+        private ListBox listBox3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Label label7;
     }
 }
